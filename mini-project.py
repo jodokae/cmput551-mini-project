42111e17cd688f3caf8125f73c48f35ce50685fd
Johannes <sir_stormrider@web.de>
2017-11-21 00:22:42 -0700

    improved console output

--- mini-project.py
+++ mini-project.py
@@ -3,16 +3,32 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import scale
 from sklearn.metrics import confusion_matrix
+<<<<<<< HEAD
+from prettytable import PrettyTable
+
+print ('Loading Data')
+=======
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
 
 A = np.load('A.npy')
 y = np.load('y.npy')
 
+<<<<<<< HEAD
+print ('Encoding Data')
+
+=======
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
 imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imp.fit(A)
 
 A_missingEncoded = imp.transform(A)
 #scale(A_missingEncoded)
 
+<<<<<<< HEAD
+print ('Splitting Data')
+
+=======
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
 (A_learn, A_test) = np.split(A_missingEncoded, 2)
 (y_learn, y_test) = np.split(y, 2)
 
@@ -33,10 +49,19 @@ y_pred = gnb.predict(A_test)
 print("Number of mislabeled points out of a total %d points : %d" % (A_test.shape[0],(y_test != y_pred).sum()))
 
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+<<<<<<< HEAD
+x = PrettyTable()
+x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
+x.add_row(["True (Pred)",tp, fp, tp+fp])
+x.add_row(["False (Pred)",fn, tn, fn + tn])
+x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
+print(x)
+=======
 print(tp)
 print(fp)
 print(fn)
 print(tn)
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
 
 from sklearn import tree
 
@@ -50,10 +75,19 @@ y_pred = clf.predict(A_test)
 
 print("Number of mislabeled points out of a total %d points : %d" % (A_test.shape[0],(y_test != y_pred).sum()))
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+<<<<<<< HEAD
+x = PrettyTable()
+x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
+x.add_row(["True (Pred)",tp, fp, tp+fp])
+x.add_row(["False (Pred)",fn, tn, fn + tn])
+x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
+print(x)
+=======
 print(tp)
 print(fp)
 print(fn)
 print(tn)
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
 
 print('\n Neural Net \n\n')
 
@@ -73,7 +107,16 @@ y_pred = clf.predict(A_test)
 
 print("Number of mislabeled points out of a total %d points : %d" % (A_test.shape[0],(y_test != y_pred).sum()))
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+<<<<<<< HEAD
+x = PrettyTable()
+x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
+x.add_row(["True (Pred)",tp, fp, tp+fp])
+x.add_row(["False (Pred)",fn, tn, fn + tn])
+x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
+print(x)
+=======
 print(tp)
 print(fp)
 print(fn)
 print(tn)
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
