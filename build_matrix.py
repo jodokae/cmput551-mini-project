a486bdaac665bde9e3c03227d1e0d7fce4ef9e6c
Johannes <sir_stormrider@web.de>
2017-11-21 00:22:17 -0700

    updated query and matrix build, commit small (100k rows) matrix

--- build_matrix.py
+++ build_matrix.py
@@ -4,6 +4,8 @@ import numpy as np
 import time
 import datetime
 
+print('Running Query...')
+
 with open('query.sql', 'r') as queryFile:
     query=queryFile.read().replace('\n', ' ')
     
@@ -18,59 +20,64 @@ if rows == 0:
     exit
 
 y = np.zeros(rows)
-A = np.empty((rows,40))
+A = np.empty((rows,46))
 A.fill(np.nan)
+
+print('Building Matrix ...')
 
 for rowIndex, row in enumerate(dataSet):
     if True:
+        if ((rowIndex % 100000) == 0):
+            print('Item ' + str(rowIndex) + '/' + str(rows))
         for i in range(len(row)):
             # lang case
-            if i == 0:
+            if i == 32:
                 #print(row[i])
                 if row[i] == 'java':
-                    A[rowIndex][0] = 1
-                    A[rowIndex][1] = 0
-                    A[rowIndex][2] = 0
+                    A[rowIndex][32] = 1
+                    A[rowIndex][33] = 0
+                    A[rowIndex][34] = 0
                 if row[i] == 'javascript':
-                    A[rowIndex][0] = 0
-                    A[rowIndex][1] = 1
-                    A[rowIndex][2] = 0
+                    A[rowIndex][32] = 0
+                    A[rowIndex][33] = 1
+                    A[rowIndex][34] = 0
                 if row[i] == 'ruby':
-                    A[rowIndex][0] = 0
-                    A[rowIndex][1] = 0
-                    A[rowIndex][2] = 1
-            # num_commits in push
-            elif i == 1:
-                A[rowIndex][3] = row[i]
-            # prev commit res
-            elif i == 2:
+                    A[rowIndex][32] = 0
+                    A[rowIndex][33] = 0
+                    A[rowIndex][34] = 1
+            elif i == 33:
                 if row[i] == 'build_found':
-                    A[rowIndex][4] = 1
-                    A[rowIndex][5] = 0
-                    A[rowIndex][6] = 0
+                    A[rowIndex][35] = 1
+                    A[rowIndex][36] = 0
+                    A[rowIndex][37] = 0
                 if row[i] == 'merge_found':
-                    A[rowIndex][4] = 0
-                    A[rowIndex][5] = 1
-                    A[rowIndex][6] = 0
+                    A[rowIndex][35] = 0
+                    A[rowIndex][36] = 1
+                    A[rowIndex][37] = 0
                 if row[i] == 'no_previous_build':
-                    A[rowIndex][4] = 0
-                    A[rowIndex][5] = 0
-                    A[rowIndex][6] = 1
+                    A[rowIndex][35] = 0
+                    A[rowIndex][36] = 0
+                    A[rowIndex][37] = 1
+            #dates
+            elif i == 34 or i == 35:
+                if row[i] != None:
+                    offset = (i - 34) * 4
+                    A[rowIndex][38 + offset] = row[i].timetuple()[1] #Month
+                    A[rowIndex][39 + offset] = row[i].timetuple()[2] #Day
+                    A[rowIndex][40 + offset] = row[i].timetuple()[3] * 3600 + row[i].timetuple()[4] * 60 + row[i].timetuple()[5] # Daytime in sec
+                    A[rowIndex][41 + offset] = row[i].timetuple()[6] #Weekday
             # tr status
             elif i == 36:
                 if row[i] == 'passed':
                     y[rowIndex] = 1
                 else:
                     y[rowIndex] = 0
-            #dates
-            elif i == 25 or i == 26:
-                if row[i] != None:
-                    A[rowIndex][i+4] = 0
-                    #A[rowIndex][i+4] = time.mktime(row[i].timetuple())
+                   #A[rowIndex][i+4] = time.mktime(row[i].timetuple())
             else:
                 if row[i] != None and row[i] != '':
-                    #print(row[i])
-                    A[rowIndex][i+4] = float(row[i])
+                    A[rowIndex][i] = float(row[i])
+                    
+print('Finished')
 
 np.save('A.npy', A)
 np.save('y.npy', y)
--- query.sql
+++ query.sql
@@ -1,7 +1,11 @@
 SELECT DISTINCT
+<<<<<<< HEAD
+gh_num_commits_in_push,
+=======
 gh_lang,
 gh_num_commits_in_push,
 git_prev_commit_resolution_status,
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
 gh_team_size,
 git_num_all_built_commits,
 gh_num_issue_comments,
@@ -24,8 +28,11 @@ gh_test_cases_per_kloc,
 gh_asserts_cases_per_kloc,
 gh_by_core_team_member,
 gh_description_complexity,
+<<<<<<< HEAD
+=======
 gh_pushed_at,
 gh_build_started_at,
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
 tr_duration,
 tr_log_bool_tests_ran,
 tr_log_bool_tests_failed,
@@ -35,7 +42,17 @@ tr_log_num_tests_run,
 tr_log_num_tests_skipped,
 tr_log_testduration,
 tr_log_buildduration,
+<<<<<<< HEAD
+gh_lang,
+git_prev_commit_resolution_status,
+gh_pushed_at,
+gh_build_started_at,
+tr_status
+FROM travistorrent_8_2_2017
+WHERE tr_status = "errored" OR tr_status = "failed" OR tr_status = "passed";
+=======
 tr_status
 FROM travistorrent_8_2_2017
 WHERE tr_status = "errored" OR tr_status = "failed" OR tr_status = "passed"
 LIMIT 100000;
+>>>>>>> a7d033adf2fc995315d30f4a8c5c3c751fb4a742
