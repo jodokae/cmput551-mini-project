8e356c46f8080c83c493993d6dc97fe2eaab962d
Johannes <sir_stormrider@web.de>
2017-11-21 22:32:13 -0700

    use all parameters for NN

--- mini-project.py
+++ mini-project.py
@@ -102,13 +102,11 @@ for split in range(len(X_learn)):
 
     print('\n Neural Net \n\n')
 
-    clf = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
-           beta_1=0.9, beta_2=0.999, early_stopping=False,
-           epsilon=1e-08, hidden_layer_sizes=(15,), learning_rate='constant',
-           learning_rate_init=0.001, max_iter=200, momentum=0.9,
-           nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
-           solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
-           warm_start=False)
+    clf = MLPClassifier(hidden_layer_sizes=(100, ), activation='relu', solver='adam', alpha=0.0001, 
+            batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, 
+            max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, 
+            momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, 
+            beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
     clf.fit(X_learn[split], y_learn[split])
     y_pred = clf.predict(X_test[split])
