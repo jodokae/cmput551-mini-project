b9ca96f5350ee8e0ece58d4fc334bc0e89c79a1d
Johannes <sir_stormrider@web.de>
2017-11-25 00:24:45 -0700

    added parameter learning

--- src/mini-project.py
+++ src/mini-project.py
@@ -1,13 +1,41 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import scale
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import StratifiedShuffleSplit
 from prettytable import PrettyTable
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score   
+import itertools
+
+def dict_product(dicts):
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
+
+def doStuff(learner, X_learn, y_learn, X_test, y_test):
+    learner.fit(X_learn, y_learn)
+
+    y_pred = learner.predict(X_test)
+    
+    #printRes(y_test, y_pred)
+    
+    return accuracy_score(y_test, y_pred)
+    
+    
+def printRes(y_test, y_pred):
+    print("Number of mislabeled points out of a total %d points : %d" % (y_test.shape[0],(y_test != y_pred).sum()))
+
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    x = PrettyTable()
+    x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
+    x.add_row(["True (Pred)",tp, fp, tp+fp])
+    x.add_row(["False (Pred)",fn, tn, fn + tn])
+    x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
+    print(x)
+    
+    print('Acc: ' + str(accuracy_score(y_test, y_pred)))
+
 
 print ('Loading Data')
 
@@ -23,7 +51,9 @@ X = imp.transform(A)
 
 print ('Splitting Data')
 
-sss = StratifiedShuffleSplit(n_splits=3, test_size=0.1, random_state=0)
+numruns = 2
+
+sss = StratifiedShuffleSplit(n_splits=numruns, test_size=0.1, random_state=0)
 
 X_learn = []
 X_test = []
@@ -37,87 +67,128 @@ for train_index, test_index in sss.split(X, y):
     y_test.append(y[test_index])
 
 
+treeParams = list(dict_product(dict(
+    criterion=['gini'],
+    splitter=['random'],
+    random_state=[None],
+    max_depth=[1, 10, 100], 
+    #min_samples_split=[1e-5, 1e-4, 1e-3], 
+    #min_samples_leaf=[1e-5, 1e-4, 1e-3],
+    #min_weight_fraction_leaf=[1e-5, 1e-4, 1e-3],
+    max_features=['sqrt', 'log2'],
+    #max_leaf_nodes=[100, 1000, 10000]
+)))
+nnParams = list(dict_product(dict(
+    activation=['logistic'],
+    hidden_layer_sizes=[(10,), (50,), (100,)],
+    #alpha=[1e-4, 1e-3, 1e-2],
+    #learning_rate_init=[1e-3, 1e-2, 1e-1],
+    #beta_1=[0.1, 0.5, 0.9], 
+    #beta_2=[0.1, 0.5, 0.9]
+))) 
+
+
+numMeanParams = 1
+numBayesParams = 1
+numTreeParams = len(treeParams)
+numNnParams = len(nnParams)
+
+meanAcc = np.zeros((numruns, numMeanParams))
+bayesAcc = np.zeros((numruns, numBayesParams))
+treeAcc = np.zeros((numruns, numTreeParams))
+nnAcc = np.zeros((numruns, numNnParams))
+
 print ('Comp: 0 / 1: ' + str((y == 0).sum() / (y == 1).sum()))
 
-for split in range(len(X_learn)):
+for split in range(numruns):
+
+    print('Run ' + str(split) + '/' + str(numruns))
     
-    print('\n Mean \n\n')
+    
+    #print('\n Mean \n\n')
 
     pred = 0 if ((y_learn[split] == 0).sum()) > ((y_learn[split] ==1).sum()) else 1
-    
     y_pred = np.zeros(y_test[split].shape) + pred
     
-    print("Number of mislabeled points out of a total %d points : %d" % (X_test[split].shape[0],(y_test[split] != y_pred).sum()))
-    tn, fp, fn, tp = confusion_matrix(y_test[split], y_pred).ravel()
-    x = PrettyTable()
-    x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
-    x.add_row(["True (Pred)",tp, fp, tp+fp])
-    x.add_row(["False (Pred)",fn, tn, fn + tn])
-    x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
-    print(x)
+    meanAcc[split][0] = accuracy_score(y_test[split], y_pred) 
     
-    print('Acc: ' + str(accuracy_score(y_test[split], y_pred)))
+    #printRes(y_test[split], y_pred)
     
         
-    print('\n Gaussian Naive Bayes \n\n')
+    #print('\n Gaussian Naive Bayes \n\n')
 
     gnb = GaussianNB()
-    gnb.fit(X_learn[split], y_learn[split])
+    bayesAcc[split][0] = doStuff(gnb, X_learn[split], y_learn[split], X_test[split], y_test[split])
 
-    print(gnb.get_params())
-
-    y_pred = gnb.predict(X_test[split])
-    print("Number of mislabeled points out of a total %d points : %d" % (X_test[split].shape[0],(y_test[split] != y_pred).sum()))
-
-    tn, fp, fn, tp = confusion_matrix(y_test[split], y_pred).ravel()
-    x = PrettyTable()
-    x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
-    x.add_row(["True (Pred)",tp, fp, tp+fp])
-    x.add_row(["False (Pred)",fn, tn, fn + tn])
-    x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
-    print(x)
     
-    print('Acc: ' + str(accuracy_score(y_test[split], y_pred)))
-
-    print('\n Decision Tree \n\n')
-
-    clf = tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=2, 
-            min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, 
-            min_impurity_decrease=0.0, min_impurity_split=None, class_weight=None, presort=False)
     
-    clf = clf.fit(X_learn[split], y_learn[split])
-
-    y_pred = clf.predict(X_test[split])
-
-    print("Number of mislabeled points out of a total %d points : %d" % (X_test[split].shape[0],(y_test[split] != y_pred).sum()))
-    tn, fp, fn, tp = confusion_matrix(y_test[split], y_pred).ravel()
-    x = PrettyTable()
-    x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
-    x.add_row(["True (Pred)",tp, fp, tp+fp])
-    x.add_row(["False (Pred)",fn, tn, fn + tn])
-    x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
-    print(x)
+    
+    #print('\n Decision Tree Gini \n\n')   
+    
+    for index, params in enumerate(treeParams):
+        clf = DecisionTreeClassifier(**params)
+    
+        #print(clf.get_params())
+   
+        acc = doStuff(clf, X_learn[split], y_learn[split], X_test[split], y_test[split])
         
-    print('Acc: ' + str(accuracy_score(y_test[split], y_pred)))
-
-    print('\n Neural Net \n\n')
-
-    clf = MLPClassifier(hidden_layer_sizes=(100, ), activation='relu', solver='adam', alpha=0.0001, 
-            batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, 
-            max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, 
-            momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, 
-            beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-
-    clf.fit(X_learn[split], y_learn[split])
-    y_pred = clf.predict(X_test[split])
-
-    print("Number of mislabeled points out of a total %d points : %d" % (X_test[split].shape[0],(y_test[split] != y_pred).sum()))
-    tn, fp, fn, tp = confusion_matrix(y_test[split], y_pred).ravel()
-    x = PrettyTable()
-    x.field_names = ["", "True (Real)", "False (Real)", "Sum"]
-    x.add_row(["True (Pred)",tp, fp, tp+fp])
-    x.add_row(["False (Pred)",fn, tn, fn + tn])
-    x.add_row(["Sum", tp+fn, fp+tn, tp+fn+tn+fp])
-    print(x)
+        print(str(index+1) + '/' + str(numTreeParams) + ': ' + str(acc))
+        
+        treeAcc[split][index] = acc
+        
+    #print(clf.decision_path(X_test[split][0].reshape((1, X_test[split].shape[1]))))
     
-    print('Acc: ' + str(accuracy_score(y_test[split], y_pred)))
+        
+    
+    #print('\n Neural Net \n\n')
+    
+    for index, params in enumerate(nnParams):
+        clf = MLPClassifier(**params)
+    
+        #print(clf.get_params())
+   
+        acc = doStuff(clf, X_learn[split], y_learn[split], X_test[split], y_test[split])
+        
+        print(str(index+1) + '/' + str(numNnParams) + ': ' + str(acc))
+        
+        nnAcc[split][index] = acc
+    
+
+meanTreeAcc = np.mean(treeAcc, axis=0)
+
+bestTreeAcc = 0
+bestTreeFeat = {}
+for i, mean in enumerate(meanTreeAcc):
+    if(mean > bestTreeAcc):
+        bestTreeAcc = mean
+        bestTreeFeat = treeParams[i]
+
+meanNnAcc = np.mean(nnAcc, axis=0)
+
+bestNnAcc = 0
+bestNnFeat = {}
+for i, mean in enumerate(meanNnAcc):
+    if(mean > bestNnAcc):
+        bestNnAcc = mean
+        bestNnFeat = nnParams[i]
+        
+meanMeanAcc = np.mean(meanAcc, axis=0)
+
+bestMeanAcc = 0
+bestMeanFeat = {}
+for i, mean in enumerate(meanMeanAcc):
+    if(mean > bestMeanAcc):
+        bestMeanAcc = mean
+        
+meanBayesAcc = np.mean(bayesAcc, axis=0)
+
+bestBayesAcc = 0
+bestBayesFeat = {}
+for i, mean in enumerate(meanBayesAcc):
+    if(mean > bestBayesAcc):
+        bestBayesAcc = mean
+
+print('Mean: Acc ' + str(bestMeanAcc))
+print('Bayes: Acc ' + str(bestBayesAcc))
+print('Tree: Acc ' + str(bestTreeAcc) + ' : ' + str(bestTreeFeat))
+print('NN: Acc ' + str(bestNnAcc) + ' : ' + str(bestNnFeat))
