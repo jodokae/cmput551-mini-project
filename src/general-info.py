b7ebd66e847a9276ad5923ef1237829f73907d89
Johannes <sir_stormrider@web.de>
2017-12-07 14:57:47 -0700

    revamps after review

--- src/general-info.py
+++ src/general-info.py
@@ -4,3 +4,9 @@ y = np.load('y.npy')
 
 print(y.shape[0])
 print ('Comp: 0 / 1: ' + str((y == 0).sum() / (y == 1).sum()))
+
+A = np.load('A.npy')
+
+missing = (np.count_nonzero(np.isnan(A), axis=0) / y.shape[0])
+for i in range(missing.shape[0]):
+    print(str(i) + ': ' + str(missing[i]))
